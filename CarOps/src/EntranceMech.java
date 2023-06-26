c00dfacef2e04366cfd7d1db36651e1b2bac0603
JonGeor <76061102+JonGeor@users.noreply.github.com>
2023-06-26 23:21:50 +0300

    Update EntranceMech.java

--- CarOps/src/EntranceMech.java
+++ CarOps/src/EntranceMech.java
@@ -15,4 +15,28 @@ public class EntranceMech extends Mechanic {
 		}
 	}
 
+	//GUI
+	public Eval createEval(Vehicle vehicle, ArrayList<Task> tasks, HashMap<Part, Integer> parts, int days) {
+		return new Eval(vehicle, tasks, parts, days);
+	}
+	
+	public Eval createEval(Vehicle vehicle, int days) {
+		return new Eval(vehicle, days);
+	}
+	
+	public Vehicle searchVehicle(String plate) {
+		for(Vehicle v : Vehicle.getVehicles())
+			if(v.getPlate().equals(plate))
+				return v;
+		return null;
+	}//Various alternatives could work depending on the fields the mechanic fills in search screen
+	
+	public Vehicle createVehicle(char c,String plate, String brand, String model, int year, double extra) {
+		if(c=='C')
+			return new Car(plate, brand, model, year);
+		if(c=='B')
+			return new Bike(plate, brand, model, year, extra);
+		return new Truck(plate, brand, model, year, extra);
+	}
+
 }
